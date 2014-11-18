464a436ed8c643fc7d2adb682c19cd3970ffe345
avocadoccme <yiransh@gmail.com>
2014-11-18 14:44:58 -0800

    11-18-2014 fmc

--- ZigZag Conversion.py
+++ ZigZag Conversion.py
@@ -3,14 +3,12 @@ class Solution:
     def convert(self, s, nRows):
         if s is None or s == "" or nRows<=0: return ""
         if len(s)<=nRows or nRows == 1: return s
-        res = ""
+        res = ""; step = 2*nRows-2
         for i in range(nRows):
-            while i < len(s):
-                res += s[i]
-                if i > 0 and i < nRows-1: 
-                    if i+2*nRows-2-2*i < len(s): 
-                        res+=s[i+2*nRows-2-2*i]
-                i+=2*nRows-2
+            for j in range(i, len(s), step):
+                res += s[j]              
+                if i > 0 and i < nRows-1 and j+step-2*i < len(s): 
+                    res+=s[j+step-2*i]
         return res
 
 if __name__ == "__main__":
